e172088cea10f8b5cd9b6a6b604df770c14d813a
Emilio Garcia-Fidalgo <emilio.garcia@uib.es>
2021-05-05 18:30:26 +0200

    First stable mapping node

---
+++ src/liodom_mapping_node.cc
@@ -0,0 +1,123 @@
+/*
+* This file is part of liodom.
+*
+* Copyright (C) 2021 Emilio Garcia-Fidalgo <emilio.garcia@uib.es> (University of the Balearic Islands)
+*
+* liodom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* liodom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with liodom. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// ROS
+#include <ros/ros.h>
+#include <tf_conversions/tf_eigen.h>
+#include <tf/transform_listener.h>
+#include <sensor_msgs/PointCloud2.h>
+
+// Liodom
+#include <liodom/defs.h>
+#include <liodom/map.h>
+
+// Mapper
+liodom::Map* mapper;
+std::string fixed_frame;
+
+// ROS 
+ros::Subscriber pc_subs;
+ros::Publisher pc_pub;
+ros::WallTimer timer;
+ros::WallTime last_pub_time;
+tf::TransformListener* tf_listener;
+
+void lidarClb(const sensor_msgs::PointCloud2ConstPtr& lidar_msg) {
+  
+  // Converting ROS message to PCL
+  liodom::PointCloud::Ptr pc_new(new liodom::PointCloud);
+  pcl::fromROSMsg(*lidar_msg, *pc_new);
+  ROS_INFO_STREAM("Received cloud with " << pc_new->points.size() << " points.");
+
+  // Waiting for the current position
+  tf::StampedTransform transform;
+  try {
+    tf_listener->waitForTransform(fixed_frame, lidar_msg->header.frame_id, lidar_msg->header.stamp, ros::Duration(5.0));
+    tf_listener->lookupTransform(fixed_frame, lidar_msg->header.frame_id, lidar_msg->header.stamp, transform);
+  } catch (tf::TransformException ex) {
+    ROS_ERROR("%s",ex.what());
+    ros::Duration(1.0).sleep();    
+  }
+
+  // Converting to an Eigen Isometry3D
+  Eigen::Isometry3d trans;
+  tf::transformTFToEigen(transform, trans);
+
+  // Updating the map with the current PC
+  mapper->updateMap(pc_new, trans);
+
+  // Publishing the current map  
+  if (pc_pub.getNumSubscribers() > 0) {
+    liodom::PointCloud::Ptr map = mapper->getMap();
+    sensor_msgs::PointCloud2 cloud_msg;
+    pcl::toROSMsg(*map, cloud_msg);
+    cloud_msg.header.frame_id = fixed_frame;
+    pc_pub.publish(cloud_msg);
+  }
+
+  last_pub_time = ros::WallTime::now();
+}
+
+void timerClb(const ros::WallTimerEvent&) {
+  // Publish the accumulated cloud if last publication was more than 5 seconds before.
+  ros::WallDuration elapsed_time = ros::WallTime::now() - last_pub_time;
+  if (pc_pub.getNumSubscribers() > 0 && elapsed_time.toSec() > 5.0) {
+    liodom::PointCloud::Ptr map = mapper->getMap();
+    sensor_msgs::PointCloud2 cloud_msg;
+    pcl::toROSMsg(*map, cloud_msg);
+    cloud_msg.header.frame_id = fixed_frame;
+    pc_pub.publish(cloud_msg);
+  }
+}
+
+int main(int argc, char** argv) {
+  
+  // Initializing node
+  ros::init(argc, argv, "liodom_mapping");
+  ros::NodeHandle nh("~");
+
+  // Reading parameters
+  double voxel_xysize;
+  nh.param("voxel_xysize", voxel_xysize, 40.0);
+  ROS_INFO("Voxel size (XY): %.2f", voxel_xysize);
+
+  double voxel_zsize;
+  nh.param("voxel_zsize", voxel_zsize, 50.0);
+  ROS_INFO("Voxel size (Z): %.2f", voxel_zsize);
+
+  double reso;
+  nh.param("resolution", reso, 0.4);
+  ROS_INFO("Resolution: %.2f", reso);
+
+  nh.param("fixed_frame", fixed_frame, std::string("world"));
+
+  mapper = new liodom::Map(voxel_xysize, voxel_zsize, reso);
+
+  // Subscribers  
+  pc_subs = nh.subscribe("points", 1, lidarClb);
+  pc_pub = nh.advertise<sensor_msgs::PointCloud2>("map", 1, true);
+  timer = nh.createWallTimer(ros::WallDuration(3.0), timerClb);
+  // Listener
+  tf_listener = new tf::TransformListener();
+
+  // Receiving messages
+  ros::spin();
+  
+  return 0;
+}
---
+++ src/map.cc
@@ -0,0 +1,141 @@
+/*
+* This file is part of liodom.
+*
+* Copyright (C) 2021 Emilio Garcia-Fidalgo <emilio.garcia@uib.es> (University of the Balearic Islands)
+*
+* liodom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* liodom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with liodom. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <liodom/map.h>
+
+namespace liodom {
+
+Cell::Cell() :
+  points_(new PointCloud),
+  modified_(false) {
+    // TODO Prereserve memory according to a maximum number of points.
+}
+
+Cell::Cell(const PointCloud::Ptr& pc) :
+  points_(new PointCloud),
+  modified_(true) {
+    // TODO Prereserve memory according to a maximum number of points.
+    pcl::copyPointCloud(*pc, *points_);
+}
+
+Cell::~Cell() {
+  points_->clear();
+}
+
+void Cell::addPoint(const Point& p) {
+  points_->push_back(p);
+  modified_ = true;
+}
+
+void Cell::addPoints(const PointCloud::Ptr& pc) {
+  *points_ += *pc;
+  modified_ = true;
+}
+
+void Cell::clear() {
+  points_->clear();
+  modified_ = true;
+}
+
+void Cell::filter(pcl::VoxelGrid<Point>& filt) {
+  filt.setInputCloud(points_);
+  filt.filter(*points_);
+  modified_ = false;
+}
+
+bool Cell::modified() {
+  return modified_;
+}
+
+PointCloud::Ptr Cell::getPoints() {
+  return points_;
+}
+
+Map::Map(const double xy_size, const double z_size, const double res) :
+  voxel_xysize_(xy_size),
+  inv_voxel_xysize_(1.0 / xy_size),
+  voxel_xysize_half_(xy_size / 2.0),
+  voxel_zsize_(z_size),
+  inv_voxel_zsize_(1.0 / z_size),
+  voxel_zsize_half_(z_size / 2.0),
+  resolution_(res) {
+
+    // VoxelGrid filter
+	  vgrid_filter_.setLeafSize(resolution_, resolution_, resolution_);
+}
+
+Map::~Map() {
+  // Delete memory allocated dynamically
+  for (size_t i = 0; i < cells_vector_.size(); i++) {
+    delete cells_vector_[i];
+  }
+}
+
+void Map::updateMap(const PointCloud::Ptr& pc_in, const Eigen::Isometry3d& pose) {
+
+  // Compute world coordinates
+  PointCloud::Ptr points_map(new PointCloud);
+  pcl::transformPointCloud(*pc_in, *points_map, pose.matrix());
+
+  // Process each point
+  for (size_t i = 0; i < points_map->points.size(); i++) {
+
+    // Get the current point
+		Point point = points_map->points[i];
+
+		// Compute the corresponding cell
+    int voxel_x = int(std::floor(point.x * inv_voxel_xysize_) * voxel_xysize_ + (voxel_xysize_half_));
+    int voxel_y = int(std::floor(point.y * inv_voxel_xysize_) * voxel_xysize_ + (voxel_xysize_half_));
+    int voxel_z = int(std::floor(point.z * inv_voxel_zsize_) * voxel_zsize_ + (voxel_zsize_half_));
+    
+    // Check if the voxel is already in the map
+    HashKey key(voxel_x, voxel_y, voxel_z);
+    Cell* cell;
+    if (cells_.find(key) == cells_.end()) {
+      // Cell is not in the map, so it is added
+      cell = new Cell();
+      cells_[key] = cell;
+      cells_vector_.push_back(cell);
+    } else {
+      // Otherwise we get the corresponding cell
+      cell = cells_[key];
+    }
+
+    cell->addPoint(point);
+  }
+
+  // Update every modified cell
+  for (size_t i = 0; i < cells_vector_.size(); i++) {
+    if (cells_vector_[i]->modified()) {
+      cells_vector_[i]->filter(vgrid_filter_);
+    }
+  }
+}
+
+PointCloud::Ptr Map::getMap() {
+  PointCloud::Ptr total_points(new PointCloud);
+  // Get PCs from every cell and concatenate them
+  for (size_t i = 0; i < cells_vector_.size(); i++) {
+    *total_points += *(cells_vector_[i]->getPoints());
+  }
+
+  return total_points;
+}
+
+}  // namespace liodom
